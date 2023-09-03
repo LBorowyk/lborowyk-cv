5c83594cf4789eb93827238fa524946d5a5c9b16
LBorowyk <lborowyk@gmail.com>
2023-09-03 11:30:09 +0300

    8 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,21 @@
 import { Component } from '@angular/core';
-import {AppService} from "./app.service";
-import {StudyPlace, WorkPlace} from "./models/cv.data.model";
+import { AppService } from './app.service';
+import { StudyPlace, WorkPlace } from './models/cv.data.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'lborowyk-cv';
 
-  public getEducationDescription = (item: StudyPlace) => `(${item.degree})`
+  public getEducationDescription = (item: StudyPlace) => `(${item.degree})`;
   public getWorkDescription = (item: WorkPlace) => item.rank;
 
-  constructor(
-    public dataService: AppService,
-  ) {
+  constructor(public dataService: AppService) {}
+
+  public onPrintButtonClick() {
+    window.print();
   }
 }
